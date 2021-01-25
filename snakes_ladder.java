6af4cf974c952494582abb2768975498e6884b43
kaushiki <kaushiki.ak88@gmail.com>
2021-01-25 13:55:46 +0530

    Welcome message

---
+++ snakes_ladder.java
@@ -0,0 +1,9 @@
+public class snakes_ladder{
+   private void play(){
+      System.out.println("Snack and Ladder game played with single player at starts with position 0");
+   }
+   public static void main(String[] args){
+      snakes_ladder game = new snakes_ladder();
+      game.play();
+   }
+}
