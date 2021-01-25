89473b45211500ee124571d5643d74883f10c22c
kaushiki <kaushiki.ak88@gmail.com>
2021-01-25 14:07:31 +0530

    Player moves

--- snakes_ladder.java
+++ snakes_ladder.java
@@ -3,6 +3,20 @@ public class snakes_ladder{
       System.out.println("Snack and Ladder game played with single player at starts with position 0");
    int rolls=(int) (Math.floor(Math.random()*10 % 6 + 1));
       System.out.println("rolls"+ rolls);
+
+		int step=(int) (Math.random()*rolls % 3);
+
+		if (step == 0){
+         System.out.println("no play player stays in the same place");
+      }
+      else if(step == 1){
+         System.out.println("Ladder: player moves ahead");
+         rolls++;
+      }
+      else{
+         System.out.println("Snake: player should move back");
+         rolls--;
+      }
 	}
    public static void main(String[] args){
       snakes_ladder game = new snakes_ladder();
