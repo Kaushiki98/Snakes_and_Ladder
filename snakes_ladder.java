cd1fe5812b11f79bfa93e72f79da883bd65d0fc3
Kaushiki <69245652+Kaushiki98@users.noreply.github.com>
2021-01-27 09:10:10 +0530

    Update snakes_ladder.java

--- snakes_ladder.java
+++ snakes_ladder.java
@@ -1,7 +1,7 @@
 public class snakes_ladder{
    private void play(){
       System.out.println("Snack and Ladder game played with single player at starts with position 0");
-   int rolls=(int) (Math.floor(Math.random()*10 % 6 + 1));
+      int rolls=(int) (Math.floor(Math.random()*10 % 6 + 1));
       System.out.println("rolls"+ rolls);
 	}
    public static void main(String[] args){
